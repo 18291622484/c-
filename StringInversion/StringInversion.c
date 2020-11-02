bb1161f7af3b151740fce4be45494d232b8f8aa8
18291622484 <18291622484@163.com>
2020-11-02 15:01:29 +0800

    做了一些修改，在元素逆置的时候，使用标记法和指针的特性，让交换更加简单，不需添加太多没必要元素

--- StringInversion/StringInversion.c
+++ StringInversion/StringInversion.c
@@ -1,23 +1,30 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 int reverse_string(char* arr) {
-	if (arr[0] == '\0') {
-		return 0;
+	if (strlen(arr)<2) {
+		return arr;
 	}
 	else {
+		char ch = *arr;
+		int len = strlen(arr) - 1;
+		*arr = *(arr + len);
+		*(arr + len) = '\0';
 		reverse_string(arr + 1);
-		printf("%c", arr[0]);
-		return 0;
+		//printf("%c", arr[0]);
+		*(arr+len) = ch;
+		return arr;
 	}
 }
 
 int main() {
-	char arr[10] = "0";
-	printf("ÇëÊäÈë×Ö·û´®¸öÊý£º\n");
-	int n = 0;
-	scanf("%d", &n);
+	char arr[4] = "0";
+	//printf("ÇëÊäÈë×Ö·û´®¸öÊý£º\n");
+	//int n = 0;
+	
+	//scanf("%d", &n);
 	printf("ÇëÊäÈë×Ö·û´®£º\n");
 
 	/*for (int i = 0; i < n; i++) {
@@ -33,5 +40,6 @@ int main() {
 	//printf("%s", arr);
 	//char arr[] = "yevoli";
 	reverse_string(arr);
+	printf("%s\n", arr);
 	return 0;
 }
