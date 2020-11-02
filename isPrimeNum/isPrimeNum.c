56f0ff4f05091175a796f90b775009039556f31c
18291622484 <18291622484@163.com>
2020-11-02 11:27:45 +0800

    在判断n是否是素数时，只需判断n能否被2~sqrt（n）整除即可，所以不必判断是否被2~n-1整除，那么多次

--- isPrimeNum/isPrimeNum.c
+++ isPrimeNum/isPrimeNum.c
@@ -1,12 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
+#include <math.h>
 
-int isPrimeNum(int num) {
+int isPrimeNum(int num,int nums) {
 	if (num == 0 || num == 1) {
 		return 0;
 	}
-	for (int i = 2; i < num; i++) {
+	for (int i = 2; i <= nums; i++) {
 		if (num % i == 0) {
 			return 0;
 		}
@@ -17,15 +18,18 @@ int isPrimeNum(int num) {
 int main() {
 	printf("请输入一个数：\n");
 	int num = 0;
+	int nums = 0;
 	scanf("%d", &num);
-	if (isPrimeNum(num) == 0) {
+	nums = (int)sqrt((double)num);
+	if (isPrimeNum(num,nums) == 0) {
 		printf("不是素数！\n");
 	}
 	else {
 		printf("是素数！\n");
 	}
 	for (int i = 100; i <= 200; i++) {
-		if (isPrimeNum(i) == 1) {
+		nums = (int)sqrt((double)i);
+		if (isPrimeNum(i,nums) == 1) {
 			printf("%d ",i);
 		}
 	}
