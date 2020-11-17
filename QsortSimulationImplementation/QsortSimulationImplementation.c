a5a1889fdf745fb4403d3a09138cb5e84766427d
18291622484 <18291622484@163.com>
2020-11-17 09:58:55 +0800

    对模拟实现通用的冒泡排序进行了改进 可以用户确定自定义排序方式，升序/降序 也可以对不同类型的数组进行排序

--- QsortSimulationImplementation/QsortSimulationImplementation.c
+++ QsortSimulationImplementation/QsortSimulationImplementation.c
@@ -1,47 +1,48 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef void(*CMP)(int*, int*);
 
-CMP Bubbup(int* m, int* n) {
-	if (*m > * n) {
-		int t = 0;
-		t = *m;
-		*m = *n;
-		*n = t;
-		}
+int cmpup(int* a, int* b) {
+	return *a - *b;
 }
-CMP Bubbdown(int* m, int* n) {
-	if (*m < * n) {
-		int t = 0;
-		t = *m;
-		*m = *n;
-		*n = t;
-	}
+int cmpdown(int* a, int* b) {
+	return *b - *a;
 }
 
-void BubbleSort(int* arr,int size,CMP cmp) {
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size - i; j++) {
-			cmp(&arr[j], &arr[j + 1]);
+void myBubble(void* base, size_t size, size_t num, int (*CMP)(const void*, const void*)) {
+	//使用char可以进行一个字节一个字节的加减，只要传入对应数组的类型字节大小
+	//就可以实现，通用数组类型的冒泡排序
+	char* st = (char*)base;
+	//用来临时交换的数组，个的空见要足够大，不要pia气，要防止意外出现
+	char tmp[16];
+	for (int i = 0; i < num - 1; i++) {
+		for (int j = 0; j < num - i - 1; j++) {
+			if (CMP(st+j*size,st+(j+1)*size) > 0) {
+				//memcpy(void* destin,void* source,unsigned n)函数，从源source中拷贝n个字节到目标destin中
+				//不知道传入数组的类型，就可以使用memcpy函数，然后根据输入类型字节大小，拷贝对应的字节
+				memcpy(tmp, st+j*size, size);
+				memcpy(st+j * size, st+(j + 1) * size, size);
+				memcpy(st+(j + 1) * size, tmp, size);
+			}
 		}
 	}
 }
 
 int main() {
 	int arr[10] = { 56,23,48,51,27,19,36,67,92,40 };
-	int size = sizeof(arr) / sizeof(arr[0])-1;
-	for (int i = 0; i <= size; i++) {
+	int size = sizeof(arr[0]);
+	int num = sizeof(arr) / sizeof(arr[0]);
+	for (int i = 0; i < num; i++) {
 		printf("%d ", arr[i]);
 	}printf("\n");
 	//升序排序
-	BubbleSort(arr, size, Bubbup);
-	for (int i = 0; i <= size; i++) {
+	myBubble(arr, size,num, cmpup);
+	for (int i = 0; i < num; i++) {
 		printf("%d ", arr[i]);
 	}printf("\n");
 	//降序排序
-	BubbleSort(arr, size, Bubbdown);
-	for (int i = 0; i <= size; i++) {
+	myBubble(arr, size, num, cmpdown);
+	for (int i = 0; i < num; i++) {
 		printf("%d ", arr[i]);
 	}printf("\n");
 	return 0;
